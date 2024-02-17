c8050f87cf4376fae83e5ee7362087a1b84439bb
eduardohen1 <eduardohmferreira@gmail.com>
2024-02-17 09:15:56 -0300

    17_02_24 criando DispatcherServlet

---
+++ WebFramework/src/main/java/br/com/ehmf/webframework/web/WebFrameworkDispatcherServlet.java
@@ -0,0 +1,18 @@
+package br.com.ehmf.webframework.web;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class WebFrameworkDispatcherServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		
+	}
+
+}
